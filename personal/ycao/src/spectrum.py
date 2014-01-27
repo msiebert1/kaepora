c5485bad823153624d203e8969018800494fb835
Yixian Cao <caoyixian@gmail.com>
2014-01-27 17:58:14 -0600

    Added the accurate matching for wavelenghs. --Y.Cao

--- personal/ycao/src/spectrum.py
+++ personal/ycao/src/spectrum.py
@@ -1,13 +1,26 @@
 # Load the data files and produce an average spectrum
 #
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-datadir = '../../../data/'
-pltdir = '../plots/'
+def halfSearch(arr,find):
+    median = 0
+    bottom = 0
+    ceil = len(arr) - 1
+    while (bottom <= ceil):
+        median = (bottom + ceil) / 2
+        temp  = arr[median]
+        if (temp == find):
+            return median 
+        else:
+            if (temp < find):
+                bottom = median + 1
+            else:
+                ceil = median - 1
+    return -1 #not found
 
-plt.yscale('log')
+#Load data
+datadir = '../../../data/'
 
 f1 = open(datadir+'sn2011by-hst+lick.flm')
 lines = f1.readlines()
@@ -22,11 +35,6 @@ for line in lines:
 
 x1 = np.array(x1)
 y1 = np.array(y1)
-x1 = x1[0:2689]
-y1 = y1[0:2689]
-mediany1 = np.median(y1)
-y1 = y1/mediany1
-p1,=plt.plot(x1,y1)
 
 f2 = open(datadir+'sn2011fe-visit3-hst.flm')
 lines = f2.readlines()
@@ -41,18 +49,36 @@ for line in lines:
 
 x2 = np.array(x2)
 y2 = np.array(y2)
+
+#Match the frequencies
+low = halfSearch(x1,x2[0])
+high = halfSearch(x1,x2[len(x2)-1])
+x1 = x1[low:high+1] #Caution: wired +1 here,different from IDL
+y1 = y1[low:high+1]
+
+#Normalize y
+mediany1 = np.median(y1)
+y1 = y1/mediany1 
+
 mediany2 = np.median(y2)
 y2 = y2/mediany2
-p2,=plt.plot(x2,y2)
 
+#Average two spectrum
 ax = x1
 ay = (y1+y2)/2
+
+#plot
+pltdir = '../plots/'
+
+plt.yscale('log')
+p1,=plt.plot(x1,y1)
+p2,=plt.plot(x2,y2)
 p3,=plt.plot(ax,ay)
 
 plt.xlabel('Wavelength [A]')
 plt.ylabel('Scaled Flux')
 plt.legend([p1,p2,p3],['SN2011BY','SN2011FE','Avarage'],
-           2)
+           4,)
 plt.savefig(pltdir+'spectrum.eps')
 plt.show()
 
