23fd4e1e3f80e28cb8afe4ce94c8ef7515edeca6
malloryconlon <conlon1@illinois.edu>
2014-02-07 12:37:54 -0600

    Added updated week 3 code; includes RMS and residual plots

--- personal/MalloryConlon/specavg.py
+++ personal/MalloryConlon/specavg.py
@@ -1,5 +1,4 @@
 
-
 import numpy as np
 import os
 import scipy
@@ -17,40 +16,42 @@ bad_files = []
 
 #Start in the data folder, and make a list of each file ending with .flm in each directory
 
-"""path = '../../data/cfa/'
+path = '../../../data/cfa/'
 
-for dirs,subdirs,files in os.walk(path):
+"""for dirs,subdirs,files in os.walk(path):
     for file in files:
         if file.endswith('.flm'):
             spectra_files.append(os.path.join(dirs,file))"""
             
 #Faster ways            
-spectra_files=glob.glob("../../data/cfa/*/*.flm")
+spectra_files=glob.glob("../../../data/cfa/*/*.flm")
                     
 #Read in data, store unreadable files (give an error when read in) in a separate array
-num = 80 # the number of spectra to analyze
-for i in range(num):
+num0 = 50 # the number of spectra to analyze
+for i in range(num0):
     try:
-        spectra_arrays.append(np.loadtxt(spectra_files[i]))
+        spectra_arrays.append(Table.read(spectra_files[i],format='ascii'))
         spectra_name.append(spectra_files[i])
     except ValueError:
         bad_files.append(spectra_files[i])
 
+num = len(spectra_arrays) # Reset the number to the number of good files
+
 #deredshift data
-parameters = Table.read('../../data/cfa/cfasnIa_param.dat',format='ascii')
+parameters = Table.read('../../../data/cfa/cfasnIa_param.dat',format='ascii')
 sn_name = parameters['col1']
 sn_z = parameters['col2']
 #old_spectrum = []
-for i in range(len(spectra_arrays)):
+for i in range(num):
     old_spectrum = spectra_arrays[i]
     z=0
     file_name = spectra_name[i]
     for j in range(len(sn_name)):
         if sn_name[j] in file_name:
 		z=sn_z[j]
-    lambda_obs =old_spectrum[:,0]
+    lambda_obs =old_spectrum['col1']
     lambda_emit= lambda_obs/(1+z)
-    spectra_arrays[i]=Table([lambda_emit,old_spectrum[:,1]],names=('col1','col2'))
+    spectra_arrays[i]=Table([lambda_emit,old_spectrum['col2']],names=('col1','col2'))
 
 #scale/truncate spectra
 wave_min=0  #arbitrary minimum of wavelength range
@@ -65,11 +66,12 @@ for i in range(len(spectra_arrays)):
         wave_max=max(spectra['col1'])
 #        print spectra_name[i]
 #print wave_min,wave_max
-wavelength = np.linspace(wave_min,wave_max,100)  #creates 100 equally spaced wavelength values between the smallest range
+
+wavelength = np.linspace(wave_min,wave_max,500)  #creates 100 equally spaced wavelength values between the smallest range
 fitted_flux=[]
 
 #generates composite spectrum
-for i in range(len(spectra_arrays)):
+for i in range(num):
     new_spectrum=spectra_arrays[i]	#declares new spectrum from list
     new_wave=new_spectrum['col1']	#wavelengths
     new_flux=new_spectrum['col2']	#fluxes
@@ -92,7 +94,8 @@ for i in range(len(wavelength)):
     RMS1_flux.append(Comp_flux[i]+math.sqrt(fluxb/num))
     RMS2_flux.append(Comp_flux[i]-math.sqrt(fluxb/num))
     scatter.append(math.sqrt(fluxb/num)/Comp_flux[i])
-#print RMS_flux
+#    if RMS1_flux[i] > 10000 : print ERROR
+#print Comp_flux,RMS1_flux
     
 
 #plot composite spectrum with RMS Spectrum on top and Residual RMS on bottom
