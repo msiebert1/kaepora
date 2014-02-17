f8e660e2c3a997bdefa5f47069abe49405a988b1
DoctorHelicopter <samuelarubin@gmail.com>
2014-02-16 21:05:41 -0600

    in progress

--- personal/SamRubin/Averages.py
+++ personal/SamRubin/Averages.py
@@ -14,41 +14,81 @@ class supernova(object):
     """Attributes can be added"""
 
 SN_Array = []
-names = np.loadtxt("input data", usecols = (0)) """need to grab all the other data too"""
-for row in names:
-    SN = supernova()
-    SN.input = row[0]
-    SN_Array.append(SN)
-print len(names), "supernovae found"
+
+#names = np.loadtxt("", usecols = (0)) #"need to grab all the other data too"
+#for row in names:
+#    SN = supernova()
+#    SN.input = row[0]
+#    SN_Array.append(SN)
+#print len(names), #"supernovae found"
 
 file_list = glob.glob("../../data/cfa/*/*.flm")
 
 con = sq3.connect('../MichaelSchubert/SNe.db')
 cur = con.cursor()
+print "Reading supernovae from database..."
 for SN in SN_Array:
-    for row in cur.execute('SELECT Filename, SN, Redshift, Phase, MinWave, MaxWave, Dm15, M B, B mMinusV M FROM Supernovae'):
+    for row in cur.execute('SELECT Filename, SN, MinWave, MaxWave FROM Supernovae'):
         SN.filename = row[0]
-        SN.name = row[1]
-        SN.redshift = row[2]
-        SN.phase = row[3]
-        SN.minwave = row[4]
-        SN.maxwave = row[5]
-        SN.dm15 = row[6]
-        SN.mb = row[7]
-        SN.bminusv = row[8]
-        SN_Array.append(SN)
+        SN.minwave = row[2]
+        SN.maxwave = row[3]
+        break    
+print len(SN_Array), "items found"
         
+#"""Adds flux to the stuff"""
+print "Matching flux data..."
+j = 1
+for SN in SN_Array[0:100]:
+    k = 1
+    for filename in file_list:   
+        if SN.filename in filename:
+            data = np.loadtxt(filename)
+            SN.wavelength = data[:,0]
+            SN.flux = data[:,1]
+            error = data[:,2]
+            if not all(x == 0.0 for x in error):
+                SN.error = error
+            break
+    print j, 'supernovae fluxed'
+    j += 1
+print "Done searching. Checking data..."   
+SN_Array = [SN for SN in SN_Array if hasattr(SN, 'error')]
+
+print "Done checking. ", len(SN_Array), "items remain"       
 
 
-high_wave = 1000
+high_wave = 4000
 low_wave = 3000
-waverange = high_wave-low_wave
-compare_spectrum = SN_Array[0]
-avg_flux = []
 
+compare_spectrum = SN_Array[0]
+
+
+"""scale"""
+print "scaling.."
+for SN in SN_Array[1:]:
+    low = SN.wavelength.index(3000)
+    high = SN.wavelength.index(4000)
+    if SN.wavelength.min >= low_wave:
+        low_wave = SN.wavelength.min
+        low = SN.wavelength.index(low_wave)
+    if SN.wavelength.max <= high_wave:
+        high_wave = SN.wavelength.max
+        high = SN.wavelength.index(high_wave)
+    SN.flux /= np.median(SN.flux)
+    if SN != SN_Array[0]:
+        factors = compare_spectrum.flux[low:high] / SN.flux[low:high]
+        scale_factor = np.mean(factors) #"""Maybe make this a weighted average"""
+        SN.flux[low:high] *= scale_factor
+        SN.error[low:high] *= scale_factor
+
+"""composite"""
+avg_flux = []
 for SN in SN_Array:
+    waverange = SN.wavelength[low:high]
     for i in range(len(waverange)):
         average = np.sum(SN.flux[i]/SN.error[i]**2 for SN in SN_Array if SN.error[i] != 0)/np.sum(1/SN.error[i]**2 for SN in SN_Array if SN.error[i] != 0 and SN.error[i] != None)
-        avg_flux.append(average)
-        compare_spectrum.flux[i] = average
+        avg_flux[i] = average
         
+        
+p1,= plt.plot(waverange, avg_flux)
+plt.show()
