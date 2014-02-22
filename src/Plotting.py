c58d000e03a48cc60de675cbdf483a4c880571a9
Novarah Kazmi <nkazmi2@linux-a2.ews.illinois.edu>
2014-02-22 14:27:08 -0600

    Two spectra in one figure

--- src/Plotting.py
+++ src/Plotting.py
@@ -13,28 +13,35 @@ from matplotlib.ticker import FuncFormatter
 ## At the top of YOUR code write the following
 # import ../../src/Plotting
 # 
-## Place after you have calculated your data and fill in information
+## Place the following after you have calculated your data 
+## fill in information
 # 
-# plot_data    = [wavelengths, avg_flux, plus, minus]       # Expects [X, Y, Pos_Y Err, Neg_Y Err]
+# plot_data_1  = [wavelength_1, Spectra_1, plus, minus]     # Expects [X, Y, Pos_Y Err, Neg_Y Err]
+# plot_data_2  = [wavelength_2, Spectra_2, plus, minus]     # Expects [X, Y, Pos_Y Err, Neg_Y Err]
 # title        = "Super Cool"                               # Within the quotes place plot title
 # image_title  = "../personal/YOUR_REP/WHOA.png"            # Name the image
 # xlabel       = "Wavelength ($\AA$)"                       # Label the X axis
-# ylabel       = "COOLLLLLL"				    # Label the Y axis
-# legend1      = "AVG Spectrum"                             # Name the title you wish to see in the legend
-# 
+# ylabel       = "Relative Flux"			    # Label the Y axis
+# legend_1     = "First Spectra"                            # Name the title to see in the legend
+# legend_2     = "Second Spectra"                           # Name second data array
+#
 ## The following line will plot the data
 #
-# Plotting.main(plot_data,title, image_title , xlabel, ylabel, legend1)
+# Plotting.main(plot_data,title, image_title , xlabel, ylabel, legend_1,legend_2)
 #
 #
 ###########################################
-def main(plot_data, title, image_title, xlabel, ylabel, legend1):
+def main(plot_data_1, plot_data_2, title, image_title, xlabel, ylabel, legend_1,legend_2):
 
 # re-name variables
-	xaxis = plot_data[0] 
-	yaxis = plot_data[1]
-	err_p = plot_data[2] 
-	err_n = plot_data[3]
+	xaxis_1 = plot_data[0] 
+	yaxis_1 = plot_data[1]
+	err_p_1 = plot_data[2] 
+	err_n_1 = plot_data[3]
+	xaxis_2 = plot_data[0] 
+	yaxis_2 = plot_data[1]
+	err_p_2 = plot_data[2] 
+	err_n_2 = plot_data[3]
 
 # Legend parameters, must come before plotting
 	params = {'legend.fontsize': 15, 'legend.linewidth':2}
@@ -44,13 +51,12 @@ def main(plot_data, title, image_title, xlabel, ylabel, legend1):
 	ax1 = subplot(1,1,1)
 
 # Read legend labels from files
-#	ax1.plot(xaxis[:],yaxis[:],'k',label = name[5] ) # (X_comp, Y_comp, 'color', curve name)
-#	ax1.plot(xaxis[:],yaxis[:],'k',label = legend1 ) # still reading in test_data
-	ax1.plot(xaxis,yaxis,'k',label = legend1 )
+	ax1.plot(xaxis_1,yaxis_1,'b',label = legend_1 ) # (X_comp, Y_comp, 'color', curve name)
+	ax1.plot(xaxis_2,yaxis_2,'g',label = legend_2 )
 
 # Error surrounds data
-#	plt.fill_between(xaxis[:],err_p[:],err_n[:],alpha=1.5, edgecolor='#000080', facecolor='#5F9EA0')
-	plt.fill_between(xaxis,err_p,err_n,alpha=1.5, edgecolor='#000080', facecolor='#5F9EA0')
+	plt.fill_between(xaxis_1,err_p_1,err_n_1,alpha=1.5, edgecolor='#000080', facecolor='#AFEEEE')
+	plt.fill_between(xaxis_2,err_p_2,err_n_2,alpha=1.5, edgecolor='#006400', facecolor='#98FB98')
 
 # Remove legend box frame 
 	l = legend()
@@ -58,8 +64,8 @@ def main(plot_data, title, image_title, xlabel, ylabel, legend1):
 	draw()
 
 #Set the visual range. Automatic range is ugly. 
-	xmin = int(float(xaxis[0]))
-	xmax = int(float(xaxis[-1]))
+	xmin = int(float(xaxis_1[0]))
+	xmax = int(float(xaxis_2[-1]))
 	plt.xlim((xmin,xmax))
 
 #Label the figure and show
@@ -69,5 +75,4 @@ def main(plot_data, title, image_title, xlabel, ylabel, legend1):
 	plt.savefig( image_title )
 
 	plt.show()
-	
 
