e5d25f67bd711c4a9bc3a4e0eca9952513507270
Novarah Kazmi <nkazmi2@linux-a2.ews.illinois.edu>
2014-02-22 14:25:39 -0600

    Plot 2 spectra in one figure

--- personal/NovarahKazmi/Plot_2.py
+++ personal/NovarahKazmi/Plot_2.py
@@ -21,7 +21,7 @@ from matplotlib.ticker import FuncFormatter
 [] stacked data (different y axis) 
 
 """
-def main(plot_data, title, image_title, xlabel, ylabel, legend1):
+def main(plot_data_1, plot_data_2, title, image_title, xlabel, ylabel, legend_1,legend_2):
 #def main(xaxis, yaxis,err_p, err_n, title, image_title, xlabel, ylabel, legend1):
 # open .txt file. Has the following: 
 # file location , title for plot, title for saved figure, x axis label, y axis label, legend label(s)
@@ -29,10 +29,14 @@ def main(plot_data, title, image_title, xlabel, ylabel, legend1):
 #	test_data = np.loadtxt( name[0] ,dtype = str)
 
 # re-name variables
-	xaxis = plot_data[0] 
-	yaxis = plot_data[1]
-	err_p = plot_data[2] 
-	err_n = plot_data[3]
+	xaxis_1 = plot_data[0] 
+	yaxis_1 = plot_data[1]
+	err_p_1 = plot_data[2] 
+	err_n_1 = plot_data[3]
+	xaxis_2 = plot_data[0] 
+	yaxis_2 = plot_data[1]
+	err_p_2 = plot_data[2] 
+	err_n_2 = plot_data[3]
 
 # Legend parameters, must come before plotting
 	params = {'legend.fontsize': 15, 'legend.linewidth':2}
@@ -44,11 +48,13 @@ def main(plot_data, title, image_title, xlabel, ylabel, legend1):
 # Read legend labels from files
 #	ax1.plot(xaxis[:],yaxis[:],'k',label = name[5] ) # (X_comp, Y_comp, 'color', curve name)
 #	ax1.plot(xaxis[:],yaxis[:],'k',label = legend1 ) # still reading in test_data
-	ax1.plot(xaxis,yaxis,'k',label = legend1 )
+	ax1.plot(xaxis_1,yaxis_1,'b',label = legend_1 )
+	ax1.plot(xaxis_2,yaxis_2,'g',label = legend_2 )
 
 # Error surrounds data
 #	plt.fill_between(xaxis[:],err_p[:],err_n[:],alpha=1.5, edgecolor='#000080', facecolor='#5F9EA0')
-	plt.fill_between(xaxis,err_p,err_n,alpha=1.5, edgecolor='#000080', facecolor='#5F9EA0')
+	plt.fill_between(xaxis_1,err_p_1,err_n_1,alpha=1.5, edgecolor='#000080', facecolor='#AFEEEE')
+	plt.fill_between(xaxis_2,err_p_2,err_n_2,alpha=1.5, edgecolor='#006400', facecolor='#98FB98')
 
 # Remove legend box frame 
 	l = legend()
@@ -56,8 +62,8 @@ def main(plot_data, title, image_title, xlabel, ylabel, legend1):
 	draw()
 
 #Set the visual range. Automatic range is ugly. 
-	xmin = int(float(xaxis[0]))
-	xmax = int(float(xaxis[-1]))
+	xmin = int(float(xaxis_1[0]))
+	xmax = int(float(xaxis_2[-1]))
 	plt.xlim((xmin,xmax))
 
 #Label the figure and show
