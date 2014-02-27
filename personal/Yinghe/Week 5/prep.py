62bdf5c6333d0e3c0f77bfea72488fc6fd76a3b4
luminosa42 <ylv4@illinois.edu>
2014-02-27 12:21:05 -0600

    current version

--- personal/Yinghe/Week 5/prep.py
+++ personal/Yinghe/Week 5/prep.py
@@ -8,6 +8,8 @@ from astroquery.irsa_dust import IrsaDust
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate as inter
+from math import floor, ceil
+import sqlite3 as sq3
 
 
 def fm_unred(wave, flux, ebv, *args, **kwargs):
@@ -204,78 +206,6 @@ def fm_unred(wave, flux, ebv, *args, **kwargs):
         ExtCurve = curve - R_V
         return flux, ExtCurve
 
-
-#list of files
-spectra_files = glob.glob ('../../../data/cfa/*/*.flm')
-
-#holds spectra data (wavelength,flux,weight)
-spectra_data = []
-#holds file pathname
-file_path = []
-junk_data = []
-
-#number of spectra to modify
-num = 20
-
-#get data, pathnames
-for i in range(num):
-	try:
-        	spectra_data.append(np.loadtxt(spectra_files[i]))
-        	file_path.append(spectra_files[i][14:-4])
-		#print file_path
-             
-	except ValueError:
-		junk_data.append(spectra_files)
-
-#update num to number of good spectra files
-num = len(spectra_data)
-
-#table containing sn names, redshifts, etc.
-sn_parameters = np.genfromtxt('../../../data/cfa/cfasnIa_param.dat',dtype = None)
-
-#holds sn name
-sn = []
-#holds redshift value
-z = []
-
-#get relevent parameters needed for calculations
-for i in range(len(sn_parameters)):
-	sn.append(sn_parameters[i][0])
-	z.append(sn_parameters[i][1])
-
-"""
-NOTE:
-Use NED
-"""
-#deredden and deredshift the spectra
-for i in range(num):#go through selected spectra data
-	for j in range(len(sn)):#go through list of SN parameters
-		if sn[j] in file_path[i]:#SN with parameter matches the path
-			print "looking at SN",sn[j]
-			ext = IrsaDust.get_extinction_table("SN%s"%sn[j])
-			print ext
-			b = 0
-			v = 0
-			for k in range(len(ext)):
-				print "looking at",ext[k][0]
-				if "B" in ext[k][0]:
-					print "found b:",ext[k][3]
-					b = ext[k][3]
-				if "V" in ext[k][0]:
-					print "found v:",ext[k][3]
-					v = ext[k][3]
-				
-			print "\n",sn[j]			
-			print "starting flux:\n",spectra_data[i][:,1]
-			print "b-v value:",b,v
-			spectra_data[i][:,1] = fm_unred(spectra_data[i][:,0],spectra_data[i][:,1],b-v,R_V=3.1)
-			print "de-reddened flux:\n",spectra_data[i][:,1]
-			print "starting wavelength:\n",spectra_data[i][:,0]
-			spectra_data[i][:,0] /= (1+z[j])
-			print "z:",z[j]
-			print "de-red-shifted wavelength:\n",spectra_data[i][:,0]	
-
-
 # data interpolation
 # pixel size: every 2 As (subject to change)
 
@@ -304,16 +234,86 @@ def Interpo(spectra) :
     #newcol = Column(fitted_flux,name = 'Flux')  
     #new.add_column(newcol,index = None)
     return new
-    
-overall = []
+
+
+#list of files
+spectra_files = glob.glob ('../../../data/cfa/*/*.flm')
+
+#holds spectra data (wavelength,flux,weight)
+spectra_data = []
+#holds file pathname
+file_path = []
+junk_data = []
+
+#number of spectra to modify
+num = 2
+
+#get data, pathnames
 for i in range(num):
-    newdata = Interpo(spectra_data[i])
+	try:
+        	spectra_data.append(np.loadtxt(spectra_files[i]))
+        	file_path.append(spectra_files[i][14:-4])
+		#print file_path
+             
+	except ValueError:
+		junk_data.append(spectra_files)
+
+#update num to number of good spectra files
+num = len(spectra_data)
+
+#table containing sn names, redshifts, etc.
+sn_parameters = np.genfromtxt('../../../data/cfa/cfasnIa_param.dat',dtype = None)
+
+#holds sn name
+sn = []
+#holds redshift value
+z = []
+
+overall = []
+
+#get relevent parameters needed for calculations
+for i in range(len(sn_parameters)):
+	sn.append(sn_parameters[i][0])
+	z.append(sn_parameters[i][1])
+
+"""
+NOTE:
+Use NED
+"""
+#deredden and deredshift the spectra
+#Using the Fitzpatrick 2007 Model.
+for i in range(num):#go through selected spectra data
+	for j in range(len(sn)):#go through list of SN parameters
+		if sn[j] in file_path[i]:#SN with parameter matches the path
+			print "looking at SN",sn[j]
+			ext = IrsaDust.get_extinction_table("SN%s"%sn[j])
+#			print ext
+			b = 0
+			v = 0
+			for k in range(len(ext)):
+				print "looking at",ext[k][0]
+				if "B" in ext[k][0]:
+					print "found b:",ext[k][3]
+					b = ext[k][3]
+				if "V" in ext[k][0]:
+					print "found v:",ext[k][3]
+					v = ext[k][3]
+				
+			print "\n",sn[j]			
+			print "starting flux:\n",spectra_data[i][:,1]
+			print "b-v value:",b,v
+			spectra_data[i][:,1] = fm_unred(spectra_data[i][:,0],spectra_data[i][:,1],b-v,R_V=3.1)
+			print "de-reddened flux:\n",spectra_data[i][:,1]
+			print "starting wavelength:\n",spectra_data[i][:,0]
+			spectra_data[i][:,0] /= (1+z[j]) # deredshift
+			print "z:",z[j]
+			print "de-red-shifted wavelength:\n",spectra_data[i][:,0]	
+
+for i in range(num) :   
+    newdata = Interpo(spectra_data[i]) # Do the interpolation
     overall.append(newdata)
 #    print newdata
-    
-
-    
-    """ For the upcoming people's reference, use table 'newdata'. """
+        
 
     # output data into a file (just for testing, no need to implement)
 #    output = 'testdata/modified-%s.dat'%(spectra_files[i][27:-4])
