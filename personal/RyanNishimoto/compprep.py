bf6de6e7718ffa9813a5a38defaee8e626afdff1
Nishimotord <nishimo1@illinois.edu>
2014-02-28 17:13:10 -0600

    combined interpolation code (under double #### lines)

--- personal/RyanNishimoto/compprep.py
+++ personal/RyanNishimoto/compprep.py
@@ -8,6 +8,7 @@ from astroquery.irsa_dust import IrsaDust
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.interpolate as inter
+from math import floor,ceil
 
 
 #list of files
@@ -21,7 +22,7 @@ file_path = []
 junk_data = []
 
 #number of spectra to modify
-num = 100
+num = 20
 
 #get data, pathnames
 for i in range(num):
@@ -94,6 +95,54 @@ for i in range(num):#go through selected spectra data
 			print "z:",z[j]
 			print "de-red-shifted wavelength:\n",spectra_data[i][:,0]
 print "\n##########\n##########\ndone de-reddening and de-redshifting"
+##############################################################################################################################################
+##############################################################################################################################################
+def Interpo(spectra) :
+    wave_min = 1000
+    wave_max = 20000
+    pix = 2
+    #wavelength = np.linspace(wave_min,wave_max,(wave_max-wave_min)/pix+1)  #creates N equally spaced wavelength values
+    wavelength = np.arange(ceil(wave_min), floor(wave_max), dtype=int, step=pix)
+    fitted_flux = []
+    fitted_error = []
+    new = []
+    #new = Table()
+    #new['col0'] = Column(wavelength,name = 'wavelength')
+    new_spectrum=spectra	#declares new spectrum from list
+    new_wave=new_spectrum[:,0]	#wavelengths
+    new_flux=new_spectrum[:,1]	#fluxes
+    new_error=new_spectrum[:,2]   #errors
+    lower = new_wave[0] # Find the area where interpolation is valid
+    upper = new_wave[len(new_wave)-1]
+    lines = np.where((new_wave>lower) & (new_wave<upper))	#creates an array of wavelength values between minimum and maximum wavelengths from new spectrum
+    indata=inter.splrep(new_wave[lines],new_flux[lines])	#creates b-spline from new spectrum
+    inerror=inter.splrep(new_wave[lines],new_error[lines]) # doing the same with the errors
+    fitted_flux=inter.splev(wavelength,indata)	#fits b-spline over wavelength range
+    fitted_error=inter.splev(wavelength,inerror)   # doing the same with errors
+    badlines = np.where((wavelength<lower) | (wavelength>upper))
+    fitted_flux[badlines] = 0  # set the bad values to ZERO !!! 
+    new = Table([wavelength,fitted_flux],names=('col1','col2')) # put the interpolated data into the new table    
+    #newcol = Column(fitted_flux,name = 'Flux')  
+    #new.add_column(newcol,index = None)
+    return new
 
 
+for i in range(num) :   
+    newdata = Interpo(spectra_data[i]) # Do the interpolation
+
+#    print newdata
+        
+
+# output data into a file (just for testing, no need to implement)
+#    output = 'testdata/modified-%s.dat'%(spectra_files[i][27:-4])
+#    ascii.write(newdata, output)
+   
+    # plot spectra (just for testing, no need to implement)
+    x = newdata['col1']
+    y = newdata['col2']
+    plt.plot(x,y)
+    
+plt.xlim(3000,7000)
+plt.show()
+plt.savefig('test.png')
 
