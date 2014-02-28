a5e1714adea24d492db8d3188050d9ddbdd45cbd
Novarah Kazmi <nkazmi2@linux-a3.ews.illinois.edu>
2014-02-28 15:18:38 -0600

    Single Data Stream

--- personal/NovarahKazmi/Plot_2.py
+++ personal/NovarahKazmi/Plot_2.py
@@ -4,41 +4,40 @@ from matplotlib import rc
 import matplotlib.pyplot as plt
 import math
 from pylab import *
-#import matplotlib.font_manage
+
 #import matplotlib.gridspec as gridspec # Doesn't exist on EWS Linux
+
+#import matplotlib.font_manage
 #from matplotlib.font_manager import FontProperties
 #from matplotlib.ticker import FuncFormatter
 
-"""
-### Tasks
-[X] what figures to plot 
-[X] atm just main light curve plot 
-[X] flexible data label 
-[X] need to account for spaces in labels
-[X] pipe functions
-[X] personalizing the plots from the data
-[] histogram
-[] zoom
+#def main(fig_type,height,plot_data_1,plot_data_2,plot_data_3,image_title,plot_labels,legend_names):
+def main(fig_type,height,Show_Data,image_title,plot_labels,legend_names):
+# Available Plots:  Relative Flux, Residuals, Spectra/Bin, Age, Delta, Redshift
+	#                    0              1          2            3    4      5
+	#Height =           [8,             2,         3,           2,   2,     2]
 
-[] Write using latex font or similar
-[] stacked data (different y axis) 
+	#Plots = [0,1,2,3,4,5] # Plots to generate
 
-"""
-def main(fig_type,plot_data_1,plot_data_2,plot_data_3,image_title,plot_labels,legend_names):
+	#h = []
+
+	#for m in Plots:
+	#    h.append(Height[m])
+
 # re-name variables
-	xaxis_1   = plot_data_1[0] 
-	yaxis_1   = plot_data_1[1]
-	err_p_1   = plot_data_1[2] 
-	err_n_1   = plot_data_1[3]
+	xaxis_1   = Show_Data[:][0][0] 
+	yaxis_1   = Show_Data[:][0][1]
+	err_p_1   = Show_Data[:][0][2] 
+	err_n_1   = Show_Data[:][0][3]
 
-	xaxis_2   = plot_data_2[0] 
-	yaxis_2   = plot_data_2[1]
-	err_p_2   = plot_data_2[2] 
-	err_n_2   = plot_data_2[3]
+	xaxis_2   = Show_Data[:][1][0]
+	yaxis_2   = Show_Data[:][1][1]
+	err_p_2   = Show_Data[:][1][2]
+	err_n_2   = Show_Data[:][1][3]
 
-	xaxis_3   = plot_data_3[0] 
-	yaxis_3_1 = plot_data_3[1]
-	yaxis_3_2 = plot_data_3[2] 
+	xaxis_3   = Show_Data[:][2][0]
+	yaxis_3_1 = Show_Data[:][2][1]
+	yaxis_3_2 = Show_Data[:][2][2]
 
 	title     = plot_labels[0]
 	xlabel    = plot_labels[1]
@@ -49,22 +48,15 @@ def main(fig_type,plot_data_1,plot_data_2,plot_data_3,image_title,plot_labels,le
 	legend_2  = legend_names[1]
 	legend_3  = legend_names[2]
 	legend_4  = legend_names[3]
-"""
-# Scale the flux. Temporary method, will try other methods. 
-#for i in range(plot_data_1):
-	yaxis_1   /= np.median(plot_data_1[:][1])
-	yaxis_2   /= np.median(plot_data_2[:][1])
-	yaxis_3_1 /= np.median(plot_data_3[:][1])
-	yaxis_3_2 /= np.median(plot_data_3[:][2])
-#	new_spectra.append([wavelength, yaxis])
-	print yaxis_3_2
-"""	
+	
 # Legend parameters, must come before plotting
 	params = {'legend.fontsize': 15, 'legend.linewidth':2}
 	plt.rcParams.update(params)
 	
 	f = figure()
 	subplots_adjust(hspace=0.001)
+	#plt.figure(num = 2, dpi = 100, figsize = [8, np.sum(h)], facecolor = 'w')
+	#gs = gridspec.GridSpec(len(Plots), 1, height_ratios = h, hspace = 0.001)
 
 # Changing font parameters. 
 	params = {'legend.fontsize': 8,  
@@ -80,37 +72,6 @@ def main(fig_type,plot_data_1,plot_data_2,plot_data_3,image_title,plot_labels,le
         'weight' : 'bold',
         'size'   : 8,
         }
-
-
-	# There is a group interpolating and de-reddening the data
-	#Interpolating
-	#interpolate(data_pos, fit_flux_pos)
-	#interpolate(data_neg, fit_flux_neg)
-	
-	#avg_flux_p = sum(fit_flux_pos)/len(data_pos)
-	#avg_flux_n = sum(fit_flux_neg)/len(data_neg)
-
-
-	#residual and RMS 
-	res_flux_p = []
-	res_flux_n = []
-
-	for i in range(len(data_pos)):
-		res_flux_p.append(fit_flux_pos[i]-avg_flux_p)
-
-	rms_p     = np.sqrt(np.mean(np.square(res_flux_p),axis = 0))
-	plus_p    = avg_flux_p + rms_p
-	minus_p   = avg_flux_p - rms_p
-	scatter_p = np.divide(rms_p,avg_flux_p)
-
-
-	for i in range(len(data_neg)):
-		res_flux_n.append(fit_flux_neg[i]-avg_flux_n)
-
-	rms_n     = np.sqrt(np.mean(np.square(res_flux_n),axis = 0))
-	plus_n    = avg_flux_n + rms_n
-	minus_n   = avg_flux_n - rms_n
-	scatter_n = np.divide(rms_n,avg_flux_n)
 
 # Types of plots. This will be changed so that the options can be automated. 
 	if fig_type == 2.3: # Plotting details for two figures with a total of three sets of data
@@ -161,8 +122,18 @@ def main(fig_type,plot_data_1,plot_data_2,plot_data_3,image_title,plot_labels,le
 
 # Should change this so that its not always the first set of data -> only works for Carbon data
 #Set the visual range. Automatic range is ugly. 
+#	wave_min = []
+#	wave_max = []
+#	for m in range(len(yaxis_1)):
+#		wave_min.append(max(yaxis_1[m][1]))
+#		wave_max.append(min(yaxis_1[m][1]))
+	
+	xmin = min(wave_min)
+	xmax = max(wave_max)
+	print xmin, xmax
+
 	xmin = int(float(xaxis_1[0]))
-	xmax = int(float(xaxis_2[-1]))
+	xmax = int(float(xaxis_1[-1]))
 	plt.xlim((xmin,xmax))
 
 #Label the figure and show
@@ -182,4 +153,61 @@ def main(fig_type,plot_data_1,plot_data_2,plot_data_3,image_title,plot_labels,le
 		plt.fill_between(xaxis_2,err_p_2,err_n_2,alpha=1.5, edgecolor='#006400', facecolor='#98FB98')
 """
 #	if num_of_plots == 2:
+"""
+# Scale the flux. Temporary method, will try other methods. 
+#for i in range(plot_data_1):
+	yaxis_1   /= np.median(plot_data_1[:][1])
+	yaxis_2   /= np.median(plot_data_2[:][1])
+	yaxis_3_1 /= np.median(plot_data_3[:][1])
+	yaxis_3_2 /= np.median(plot_data_3[:][2])
+#	new_spectra.append([wavelength, yaxis])
+	print yaxis_3_2
+	# There is a group interpolating and de-reddening the data
+	#Interpolating
+	#interpolate(data_pos, fit_flux_pos)
+	#interpolate(data_neg, fit_flux_neg)
+	
+	#avg_flux_p = sum(fit_flux_pos)/len(data_pos)
+	#avg_flux_n = sum(fit_flux_neg)/len(data_neg)
 
+
+	#residual and RMS 
+	res_flux_p = []
+	res_flux_n = []
+
+	for i in range(len(data_pos)):
+		res_flux_p.append(fit_flux_pos[i]-avg_flux_p)
+
+	rms_p     = np.sqrt(np.mean(np.square(res_flux_p),axis = 0))
+	plus_p    = avg_flux_p + rms_p
+	minus_p   = avg_flux_p - rms_p
+	scatter_p = np.divide(rms_p,avg_flux_p)
+
+
+	for i in range(len(data_neg)):
+		res_flux_n.append(fit_flux_neg[i]-avg_flux_n)
+
+	rms_n     = np.sqrt(np.mean(np.square(res_flux_n),axis = 0))
+	plus_n    = avg_flux_n + rms_n
+	minus_n   = avg_flux_n - rms_n
+	scatter_n = np.divide(rms_n,avg_flux_n)
+
+"""
+"""
+# Removes the xaxis labels
+if Plots[len(Plots)-1] != 0:
+    RFxticklabels = Rel_flux.get_xticklabels()
+    plt.setp(RFxticklabels, visible=False)
+if Plots[len(Plots)-1] != 1:
+    RSxticklabels = Resid.get_xticklabels()
+    plt.setp(RSxticklabels, visible=False)
+if Plots[len(Plots)-1] != 2:
+    SBxticklabels = SpecBin.get_xticklabels()
+    plt.setp(SBxticklabels, visible=False)
+if Plots[len(Plots)-1] != 3:
+    AGxticklabels = Age.get_xticklabels()
+    plt.setp(AGxticklabels, visible=False)
+if Plots[len(Plots)-1] != 4:
+    DLxticklabels = Delta.get_xticklabels()
+    plt.setp(DLxticklabels, visible=False)
+"""
