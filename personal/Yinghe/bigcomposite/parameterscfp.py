fdd03ec202b1ff748c1ef8c8db9612d39d777729
luminosa42 <ylv4@illinois.edu>
2014-03-14 14:02:42 -0500

    grab cfp data

--- personal/Yinghe/bigcomposite/parameterscfp.py
+++ personal/Yinghe/bigcomposite/parameterscfp.py
@@ -17,12 +17,12 @@ Table should look something like this:
 SN_name	Host_Galaxy	R(v)	Redshift	B_val	V_val	Carbon_pos/neg
 
 ##################################
-CURRENTLY ONLY DOES CFA DATA FILES
+CURRENTLY ONLY DOES CSP DATA FILES
 CURRENTLY RETRIEVES ONLY B/V VALS
 ##################################
 """
 import glob
-import astroquery
+#import astroquery
 from astroquery.irsa_dust import IrsaDust
 from astropy.table import Table
 from astropy.io import ascii
@@ -30,27 +30,41 @@ import numpy as np
 
 
 #list of files
-bsnip = glob.glob ('../../data/bsnip/*.flm')
-cfa = glob.glob ('../../data/cfa/*')
-csp = glob.glob ('../../data/csp/*.flm')
+#bsnip = glob.glob ('../../data/bsnip/*.flm')
+#cfa = glob.glob ('../../data/cfa/*')
+csp = glob.glob ('../../../data/csp/*.dat')
 
+#print csp
 #Truncates cfa pathname into needed format and removes cfa.dat files
-del cfa[0]
-del cfa[0]
-for i in range(len(cfa)):
-	cfa[i]= cfa[i][15:]
-	print cfa[i]
+#del csp[0]
+#del csp[0]
+cspmini= [] # a shorter list
+
+for i in range(len(csp)):    
+    csp[i] = 'sn20'+csp[i][20:24]
+#    print csp[i]
+        
+for sn in csp: # delete duplicate items
+    if sn not in cspmini:
+        cspmini.append(sn)
+    
+for i in range(len(cspmini)): # For single-letter SNs
+    if cspmini[i].endswith('_') :        
+        cspmini[i] = cspmini[i][:-1]
+        
+print cspmini    
+    
 b = []
 v = []
-for i in range(len(cfa)):
-	print "looking at SN",cfa[i]
-	ext = IrsaDust.get_extinction_table(cfa[i])
+for i in range(len(cspmini)):
+	print "looking at SN",cspmini[i]
+	ext = IrsaDust.get_extinction_table(cspmini[i])
 	print ext
 	print ext[1][0],ext[1][3]
 	print ext[2][0],ext[2][3]
 	b.append(ext[1][3])
 	v.append(ext[2][3])
 
-param = Table([cfa,b,v],names=('sn','B','V'))
-#ascii.write(param,'extinction.dat')
+param = Table([cspmini,b,v],names=('sn','B','V'))
+ascii.write(param,'extinctioncsp.dat')
 	
