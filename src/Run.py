087d1cfaffa957e3cba988e17dd5dafe1fc2c68c
DoctorHelicopter <samuelarubin@gmail.com>
2014-03-26 18:53:08 -0500

    Finished cleaning up code
    Also fleshed out run.py a little bit

--- src/Run.py
+++ src/Run.py
@@ -1,4 +1,46 @@
 import composite
-#import Plotting
+import Plotting
+from astropy.table import Table
 #import targeted
-table = composite.main()
+"""
+Here's the main function that anyone can use to run some analysis.
+
+The first line runs the composite program.
+This grabs the selected data from SNe.db.
+Change the input to composite.main to whatever you want your database query to be.
+This is the code that outputs a text file of data for later use.
+It also shows/saves a basic plot of the composite data and associated errors.
+
+The next section sets up the plots to be used, and then plots using Plotting.py
+Right now it doesn't work...
+
+More can be added as our programs evolve
+"""
+
+#This part works just fine
+template = composite.main("SELECT * FROM Supernovae WHERE snr > 8")
+
+Data = Table.read('../plots/TestComposite', format='ascii')
+
+#Checking to see how the table reads..right now it has a header that might be screwing things up.
+print Data
+
+#To be honest, I'm not sure entirely how this works.
+#Can someone who worked on this piece of code work with it?
+Relative_Flux = [Data[0], Data[1], template.name]  # Want to plot a composite of multiple spectrum
+Residuals     = [Data[0], Data[2], template.name]
+Spectra_Bin   = []
+Age           = []
+Delta         = []
+Redshift      = [] 
+Show_Data     = [Relative_Flux,Residuals]
+image_title  = "../plots/Composite_Spectrum_plotted.png"            # Name the image (with location)
+title        = "Composite Spectrum" 
+
+# Available Plots:  Relative Flux, Residuals, Spectra/Bin, Age, Delta, Redshift, Multiple Spectrum, Stacked Spectrum
+#                   0              1          2            3    4      5         6,                 7
+Plots = [0] # the plots you want to create
+
+# The following line will plot the data
+#It's commented out until it works...
+#Plotting.main(Show_Data , Plots, image_title , title)
--- src/composite.py
+++ src/composite.py
@@ -21,6 +21,7 @@ mn.patch()
 SN_Array = []
 full_array = []
 compare_spectrum = []
+#This only gets used if selecting by max light spectra
 #max_light = []
 #max_light = np.loadtxt("../personal/AaronBeaudoin/week4/MaxSpectra.dat", dtype = 'str', delimiter = " ", skiprows = 1)
 
@@ -36,11 +37,11 @@ con = sq3.connect('../../../SNe.db')
 #con = sq3.connect('../../temp/SNe.db')
 cur = con.cursor()
 
+#Pulls in all columns from the database for the selected query
 def grab(sql_input, Full_query):
     print "Collecting data..."
     SN_Array = []
     cur.execute(sql_input)
-    #at some point this should be more modular but for now I'm only going to accept the full query
     for row in cur:
         if sql_input == Full_query:
             SN           = supernova()
@@ -56,27 +57,24 @@ def grab(sql_input, Full_query):
 	    SN.B_minus_v = row[9]
 	    SN.targeted  = row[10]
 	    SN.SNR       = row[11]
-            #spectra = msg.unpackb(row[7])
-            #SN.spectrum = spectra
 	    interp       = msg.unpackb(row[12])
 	    SN.interp    = interp
 	    try:
 		SN.wavelength = SN.interp[0,:]
 		SN.flux       = SN.interp[1,:]
 		SN.ivar       = SN.interp[2,:]
-		
-		#print SN.flux
 	    except TypeError:
 		continue
 	    full_array.append(SN)
             SN_Array.append(SN)
-            #print SN.interp
 	else:
 	    print "Invalid query...more support will come"
     print len(SN_Array), "spectra found"
 
     #cut the array down to be more manageable
+    #Used mostly in testing, if you want the full array of whatever you're looking at, comment this line out
     SN_Array = SN_Array[0:100]
+    
     for SN in SN_Array:
 	for i in range(len(SN.flux)):
 	    if np.isnan(SN.flux[i]):
@@ -90,6 +88,7 @@ def grab(sql_input, Full_query):
 
 
 """
+#This is something that could be implemented in the future
 #Only keeps one per supernova at max light. Condition can be changed later.
 for SN in full_array:
     for row in max_light:
@@ -107,17 +106,15 @@ def find_nearest(array,value):
     idx = (np.abs(array-value)).argmin()
     return array[idx]
 
+#This is the model for how scales should be applied, used in the find_scales function
 def scale_func(vars, in_data, out_data):
     
     scale = vars[0]
-
     model = scale * in_data
-    
-#    output = (out_data-model)*ivar**0.5
     output = model
-    #needed to reshape this?
     return output[:,0]
 
+#...Finds scales
 def find_scales(SN_Array, temp_flux, temp_ivar):
     min_overlap = 300
     scales = []
@@ -127,7 +124,6 @@ def find_scales(SN_Array, temp_flux, temp_ivar):
         #grab out the flux and inverse variance for that SN
         flux = SN.flux
         ivar = SN.ivar
-        #Make the combined inverse variance function.  Zeros should multiply to get zeros
         overlap = temp_ivar * ivar
         n_overlap = len([x for x in overlap if x > 0])
 	
@@ -155,11 +151,12 @@ def find_scales(SN_Array, temp_flux, temp_ivar):
 
     return scales
 
+#Scales the data using the factors found before
 def scale_data(SN_Array, scales):
     print "Scaling..."
     for i in range(len(scales)):
 	SN_Array[i].flux *= np.abs(scales[i])
-	SN_Array[i].ivar /= (scales[i])**2  #Check this!!
+	SN_Array[i].ivar /= (scales[i])**2
 	print "Scaled at factor ", scales[i]
     return SN_Array
 
@@ -169,46 +166,51 @@ def average(SN_Array, template):
 	#print fluxes, errors
 	fluxes = []
 	ivars  = []
+	reds = []
 	for SN in SN_Array:
 	    if len(fluxes) == 0:
 		fluxes = np.array([SN.flux])
 		ivars  = np.array([SN.ivar])
-		#waves = np.array([SN.wavelength])
- 		#reds = np.array([red])
-		#ages = np.array([age])
+ 		reds = np.array([SN.redshift])
+		phases = np.array([SN.phase])
 	    else:
 		try:
 		    fluxes = np.append(fluxes, np.array([SN.flux]), axis=0)
 		    ivars  = np.append(ivars, np.array([SN.ivar]), axis=0)
-		    #waves = np.append(waves, np.array([SN.wavelength]), axis=0)
-		    #reds = np.append(reds, np.array([red]), axis = 0)
-		    #ages = np.append(ages, np.array([age]), axis = 0)
+		    reds = np.append(reds, np.array([SN.redshift]), axis = 0)
+		    phases = np.append(phases, np.array([SN.phase]), axis = 0)
 		except ValueError:
-		    print "oh god what is happening"
+		    print "This should never happen!"
 	flux_mask = np.zeros(len(fluxes[0,:]))
 	ivar_mask = np.zeros(len(fluxes[0,:]))
 	have_data = np.where(np.sum(ivars, axis = 0)>0)
 	no_data = np.where(np.sum(ivars, axis = 0)==0)
+	has_reds = np.where(reds != None)
+	has_phase = np.where(phases != None)
+	reds = reds[has_reds]
+	phases = phases[has_phase]
 	ivar_mask[no_data] = 1
 	for i in range(len(fluxes)):
 	    ivars[i,:] += ivar_mask
         template.flux = np.average(fluxes, weights=ivars, axis=0)
-	#template.flux = np.average(fluxes, axis=0)
         template.ivar = 1/np.sum(ivars, axis=0)
+	template.redshift = sum(reds)/len(reds)
+	#template.phase = sum(phases)/len(phases)
+	#phase data isn't in the database yet
 	template.ivar[no_data] = 0
+	template.name = "Composite Spectrum"
 	return template
 
-def main():
+def main(Full_query):
     SN_Array = []
     #Accept SQL query as input and then grab what we need
-    Full_query = "SELECT * FROM Supernovae WHERE Signal_Noise > 8"
+    #Full_query = "SELECT * FROM Supernovae WHERE snr > 8"
     print "SQL Query:", Full_query
-    #sql_input = str(raw_input("Enter a SQL Query---> "))
     sql_input = Full_query
 
     SN_Array = grab(sql_input, Full_query)
 
-    #finds the longest SN we have for comparison
+    #finds the longest SN we have for our initial template
     lengths = []
     for SN in SN_Array:
         lengths.append(len(SN.flux[np.where(SN.flux != 0)]))
@@ -216,13 +218,14 @@ def main():
     composite = temp[0]
 
     #scales data, makes a composite, and splices in non-overlapping data
+    #Here is where we set our wavelength range for the final plot
     wmin = 4000
     wmax = 7500
     wavemin = composite.minwave
     wavemax = composite.maxwave
 
     #finds range of useable data
-    good = np.where(len(np.where(((wavemin <= wmin) & (wavemax >= wmax)) > 100))) #& (SN.SNR>.8*max(SN.SNR)))
+    good = np.where(len(np.where(((wavemin <= wmin) & (wavemax >= wmax)) > 100)))
     template = supernova()
     template = SN_Array[good[0]]
     template = composite
@@ -244,14 +247,16 @@ def main():
 	
 	
     print "Done."
+    print "Average redshift =", template.redshift
+    #print "Average phase =", template.phase
     lowindex = np.where(template.wavelength == find_nearest(template.wavelength, wmin))
     highindex = np.where(template.wavelength == find_nearest(template.wavelength, wmax))
-    print lowindex[0], highindex[0]
     plt.plot(template.wavelength[lowindex[0]:highindex[0]], template.flux[lowindex[0]:highindex[0]])
     plt.plot(template.wavelength[lowindex[0]:highindex[0]], template.ivar[lowindex[0]:highindex[0]])
     plt.savefig('../plots/TestComposite.png')
     plt.show()
     #Either writes data to file, or returns it to user
+    #This part is still in progress
     table = Table([template.wavelength, template.flux, template.ivar], names = ('Wavelength', 'Flux', 'Variance'))
     c_file = str(raw_input("Create a file for data? (y/n)"))
     if c_file=='y':
@@ -259,8 +264,9 @@ def main():
 		#phase_min.phase_max.deltam15_min.deltam15_max. ... --avg_phase.avg_deltam15... --#SN
 		f_name = "../plots/TestComposite"
 		table.write(f_name,format='ascii')
+		return template
     else:
-		return table
+		return template
 
 if __name__ == "__main__":
     main()
