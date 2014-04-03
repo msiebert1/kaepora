70b45f5f9d6911c21fa6bf363d0960aa5224e967
Snyder005 <snyder18@illinois.edu>
2014-04-02 22:49:57 -0500

    Sky variance generation
    Found supernovae with largest wavelength range, working on finding
    scaling

--- personal/AdamSnyder/datafidelity.py
+++ personal/AdamSnyder/datafidelity.py
@@ -22,29 +22,29 @@ import matplotlib.pyplot as plt
 ## Syntax: new_y_array = gsmooth(x_array, y_array, var_y, vexp = 0.01, nsig = 5.0)
 
 def gsmooth(x_array, y_array, var_y, vexp = 0.005, nsig = 5.0):
-
+    
     # Check for non-zero variance points, and set to 1E-20
     for i in range(len(var_y)):
         if var_y[i] == 0:
             var_y[i] = 1E-20
-
+    
     # Output y-array
     new_y = np.zeros(len(x_array), float)
-
+    
     # Loop over y-array elements
     for i in range(len(x_array)):
-
+        
         # Construct a Gaussian of sigma = vexp*x_array[i]
         gaussian = np.zeros(len(x_array), float)
         sigma = vexp*x_array[i]
-
+        
         # Restrict range to +/- nsig sigma
         sigrange = np.nonzero(abs(x_array-x_array[i]) <= nsig*sigma)
         gaussian[sigrange] = (1/(sigma*sqrt(2*pi)))*np.exp(-0.5*((x_array[sigrange]-x_array[i])/sigma)**2)
-
+        
         # Multiply Gaussian by 1 / variance
         W_lambda = gaussian / var_y
-
+        
         # Perform a weighted sum to give smoothed y value at x_array[i]
         W0 = np.sum(W_lambda)
         W1 = np.sum(W_lambda*y_array)
@@ -59,7 +59,7 @@ def gsmooth(x_array, y_array, var_y, vexp = 0.005, nsig = 5.0):
 ## Optional inputs are window length (window_len) and window type (window)
 ## Syntax: new_flux_array = wsmooth(flux_array, window_len=17, window='hanning')
 
-def wsmooth(x,window_len=11,window='hanning'):
+def wsmooth(x,window_len=75,window='hanning'):
     """smooth the data using a window with requested size.
         
         This method is based on the convolution of a scaled window with the signal.
@@ -121,28 +121,30 @@ def wsmooth(x,window_len=11,window='hanning'):
 #
 # Function to generate variance for files that are missing this data
 #
-## Function gvar() generates a variance spectrum.
+## Function genivar() generates an inverse variance spectrum.
 ## Required inputs are an array of wavelengths (wavelength) and an array of corresponding fluxes (flux)
 ## Optional inputs are velocity of smoothing (vexp) [default 0.005] and number of sigma (nsig) [default 5.0]
-## genvar(wavelength, flux, float vexp = 0.005, float nsig = 5.0)
+## genivar(wavelength, flux, float vexp = 0.005, float nsig = 5.0)
 #
 
-def genvar(wavelength, flux, vexp = 0.005, nsig = 5.0):
-
+def genivar(wavelength, flux, vexp = 0.0008, nsig = 3.0):
+    
     # Create variance from sky spectrum (Will add additional code here)
-    varflux = np.zeros(len(wavelength))+1.0 # Place holder
-
+    varflux = np.ones(len(wavelength)) # Place holder
+    
     # Smooth original flux
     new_flux = gsmooth(wavelength, flux, varflux, vexp, nsig)
-
+    
     # Generate absolute value of the noise from original flux
     error = abs(flux - new_flux)
-
+    
     # Smooth noise to find the variance
-    variance = gsmooth(wavelength, error, varflux, vexp, nsig)
-
+    sm_error = gsmooth(wavelength, error, varflux, vexp, nsig)
+    
+    ivar = 1/(sm_error**2)
+    
     # Return generated variance
-    return variance
+    return ivar
 
 ############################################################################
 #
@@ -151,16 +153,16 @@ def genvar(wavelength, flux, vexp = 0.005, nsig = 5.0):
 # Optional inputs are the upper and lower limits for the ratio
 # Syntax is clip(flux_array, upper = 1.7, lower = 0.5)
 
-def clip(flux, upper = 1.1, lower = 0.9):
-
+def clip(wavelength, flux, upper = 1.9, lower = 0.1):
+    import matplotlib.pyplot as plt
     #Clip any bad data and replace it with the smoothed value.  Fine tune the ratio limits to cut more (ratio closer to one) or less (ratio farther from one) data
-
+    
     new_flux = np.zeros(len(flux))
     clipped_points = []
     
     smooth_flux = wsmooth(flux)
     ratio = flux/smooth_flux
-
+    
     for i in range(len(ratio)):
         if ratio[i] > upper:
             new_flux[i] = smooth_flux[i]
@@ -171,6 +173,9 @@ def clip(flux, upper = 1.1, lower = 0.9):
         else:
             new_flux[i] = flux[i]
 
+    plt.plot(wavelength,flux)
+    plt.plot(wavelength, new_flux)
+    plt.show()
     return new_flux, clipped_points
 
 
@@ -179,22 +184,23 @@ def clip(flux, upper = 1.1, lower = 0.9):
 # Function telluric_flag() scans data for telluric lines and flags any
 # of the indices that have telluric contamination
 # Required inputs are an array of wavelengths and an array of fluxes
-# Optional input is the limit for flagging
-# Syntax is telluric_flag(wavelength_array,flux_array, limit = 0.99)
+# Optional input is the limit for flagging. As the limit approaches 1, the
+# amount of clipping increases.
+# Syntax is telluric_flag(wavelength_array,flux_array, limit = 0.9)
 
-def telluric_flag(wavelength, flux, limit=0.9):
-
-    telluric_lines = np.loadtxt('../../personal/malloryconlon/Data_fidelity/telluric_lines.txt')
+def telluric_flag(wavelength, flux, limit=0.5):
+    import matplotlib.pyplot as plt
+    telluric_lines = np.loadtxt('../../../personal/malloryconlon/Data_fidelity/telluric_lines.txt')
 
     mi = telluric_lines[:,0]
     ma = telluric_lines[:,1]
 
-    new_flux = wsmooth(flux,window_len=35)
+    new_flux = wsmooth(flux)
 
     ratio = flux/new_flux
     telluric_clip = []
 
-    #Look at the flux/smoothed flux ratios for a given telluric absorption range as defined by the min and max arrays. If the ratio is less than the given condition, clip and replace with the smoothed flux value.
+#Look at the flux/smoothed flux ratios for a given telluric absorption range as defined by the min and max arrays. If the ratio is less than the given condition, clip and replace with the smoothed flux value.
 
     for i in range(len(wavelength)):
         for j in range(len(mi)):
@@ -203,11 +209,11 @@ def telluric_flag(wavelength, flux, limit=0.9):
                     if ratio[i] < limit:
                         telluric_clip.append(i)
 
-    plt.plot(wavelength, flux)
-    plt.plot(wavelength[telluric_clip], flux[telluric_clip], 'rD')
+    plt.plot(wavelength,flux)
+    plt.plot(wavelength[telluric_clip],flux[telluric_clip],'rD')
     plt.show()
 
-    #Return the indices of telluric absorption
+#Return the indices of telluric absorption
     return telluric_clip
 
 ############################################################################
@@ -218,22 +224,29 @@ def telluric_flag(wavelength, flux, limit=0.9):
 # inverse variance array
 # Syntax is update_variance(wavelength_array,flux_array, variance_array)
 
-def update_variance(wavelength, flux, variance):
+def update_ivar(wavelength, flux, ivar):
 
+    import matplotlib.pyplot as plt
+    
 #Determine the clipped indices
-    telluric_clip=telluric_flag(wavelength, flux)
-    fl, clipped_points=clip(flux)
+
+    telluric_clip = telluric_flag(wavelength, flux)
+
 
     for i in range(len(clipped_points)):
-            index=clipped_points[i]
-            variance[index] = 0
+            index = clipped_points[i]
+            ivar[index] = 0
 
 
     for j in range(len(telluric_clip)):
-        index=telluric_clip[j]
-        variance[index] = 0
+        index = telluric_clip[j]
+        ivar[index] = 0
+
+    plt.plot(wavelength,flux)
+    plt.plot(wavelength, ivar)
+    plt.show()
 
     #Return the updated inverse variance spectrum
-    return variance
+    return ivar
 
 
--- personal/AdamSnyder/skyvar.py
+++ personal/AdamSnyder/skyvar.py
@@ -1,13 +1,81 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import interpolate
-import scipy 
-from math import *
 import pyfits
+import math
+from scipy import interpolate
+import sqlite3 as sq3
 
-sky = pyfits.open("kecksky.fits")
-prihdr = sky[0].header
-print prihdr
+def skyvar(wave, sky = 'kecksky.fits'):
 
-#plt.plot(wavelength, flux, 'b', wavelength, new_flux, 'r')
-#plt.show()
+    if False:
+        sky = pyfits.open("kecksky.fits")
+        crval = sky[0].header['CRVAL1']
+        delta = sky[0].header['CDELT1']
+        sky_flux = sky[0].data[0]
+        print "Keck Sky used"
+        
+    else:
+        sky = pyfits.open("licksky.fits")
+        crval = sky[0].header['CRVAL1']
+        delta = sky[0].header['CDELT1']        
+        sky_flux = sky[0].data
+        print "Lick sky used"
+
+    start = crval - math.ceil(0.5*len(sky_flux)*delta)
+    stop = crval + math.ceil(0.5*len(sky_flux)*delta)
+
+    sky_wave = [(start+delta*i) for i in range(len(sky_flux))]
+
+    plt.plot(sky_wave, sky_flux)
+    plt.show()
+
+    return new_sky
+
+con = sq3.connect('../../../First/SNe.db')
+cur = con.cursor()
+maxdif = 0.0
+start = 0.0
+stop = 0.0
+lowSN = []
+highSN = []
+
+# Iterate over entire database to find the Supernovae with greatest wavelength
+for row in cur.execute('SELECT * FROM Supernovae'):
+    dif = row[6] - row[5]
+    if dif > maxdif:
+        maxdif = row[6] - row[5]
+        start = row[5]
+        stop = row[6]
+        SN = row[0]
+
+minwav = start
+maxwav = stop
+maxwavstart = 0.0
+
+for row in cur.execute('SELECT * FROM Supernovae'):
+    if row[5] < start:
+        lowSN.append(row[0])
+    if row[6] > stop:
+        highSN.append(row[0])
+    if row[5] < minwav:
+        print row[0]
+        minwav = row[5]
+    if row[6] > maxwav:
+        print row[0]
+        maxwav = row[6]
+        maxwavstart = row[5]
+        
+# Get flux data of supernovae
+
+#flux = []
+#wavelength = []
+
+# Generate continuum
+
+#residual = flux - continuum
+
+
+print SN, maxdif, start, stop
+print len(lowSN), len(highSN), minwav, maxwav, maxwavstart
+    
+
