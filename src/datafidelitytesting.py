ff221f1acbe55592a303dc2eab090f263a869553
Snyder005 <snyder18@illinois.edu>
2014-04-17 17:35:58 -0500

    Fixed datafidelity (hopefully)
    datafidelity got deleted, so I added it back

--- src/datafidelitytesting.py
+++ src/datafidelitytesting.py
@@ -10,21 +10,6 @@ SN = np.genfromtxt('../personal/AdamSnyder/sn1996C-19960217.48-fast.flm')
 wavelength = SN[:, 0]
 flux = SN[:, 1]
 
-sky = pyfits.open('../personal/AdamSnyder/kecksky.fits')
-    
-crval = sky[0].header['CRVAL1']
-delta = sky[0].header['CDELT1']
-skyflux = sky[0].data[0]
-start = crval - ceil(0.5*len(skyflux)*delta)
-stop = crval + ceil(0.5*len(skyflux)*delta)
-skywave = [(start+delta*i) for i in range(len(skyflux))]
-
-plt.plot(skywave, skyflux)
-
-print wavelength[0], wavelength[-1]
-
-plt.plot(wavelength, flux)
-
 ivar = df.genivar(wavelength, flux)
 
 ivar_new = df.clip(wavelength, flux, ivar)
